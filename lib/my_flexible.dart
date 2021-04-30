8e7aa669a0195732ba5cc2a04047d8a09f4264d8
helen.dikovitsky@gmail.com <helen.dikovitsky@gmail.com>
2021-04-30 16:16:55 +0300

    widget_commit

--- lib/my_flexible.dart
+++ lib/my_flexible.dart
@@ -5,17 +5,55 @@ class MyFlex extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Верстка теория'),
+        title: Text('Верст ка теория'),
       ),
       body: Container(
         color: Colors.grey,
-        child: Row(
+        child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            BiggerColorBox(),
-            Flexible(child: ColorBox()),
-            Flexible(child: ColorBox()),
+          Container(
+            color: Colors.red[400],
+            width: 50,
+           child: Text(
+              'Dart',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black54,
+              ),
+              softWrap: false,
+              overflow: TextOverflow.fade,
+            ),
+          ),
+            Icon(
+              Icons.favorite,
+              size: 50,
+              color: Colors.redAccent[400],
+            ),
+            Expanded(
+                child: Image.network('https://fainaidea.com/wp-content/uploads/2019/06/acastro_190322_1777_apple_streaming_0003.0.jpg')
+            ),
+            // BiggerColorBox(),
+ //           Expanded(
+ //               flex: 1,
+ //               child: ColorBox()),
+ // // ColorBox          SizedBox(
+ // //             width: 40,
+ // //           ),
+ // //           SizedBox(
+ // //             width: 100,
+ // //             height: 100,
+ // //             child: ColorBox(),
+ // //           ),
+ //            Spacer(
+ //              flex: 2,
+ //            ),
+ //           Flexible(
+ //               fit: FlexFit.tight,
+ //               flex: 6,
+ //               child: ColorBox()
+ //           ),
           ],
         ),
       ),
@@ -27,13 +65,13 @@ class ColorBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-     // width: 80,
+      width: 80,
       height: 80,
       decoration: BoxDecoration(
         color: Colors.redAccent[400],
         border: Border.all(),
       ),
-      child: Text ('1, 2, 3'),
+    //  child: Text ('1, 2, 3'),
     );
   }
 }
@@ -42,7 +80,7 @@ class BiggerColorBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
+      width: 100,
       height: 80,
       decoration: BoxDecoration(
         color:Colors.green[500],
